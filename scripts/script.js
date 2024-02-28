8ec9c2f81b0a211b7aa57c222af655cde84ee262
matheus kamers <mkamersandrade@gmail.com>
2024-02-27 23:19:41 -0300

    Ex 8 - Manipulação do DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-alert('Bem vindo ao meu site')
+// alert('Bem vindo ao meu site')
+
+function revelarApelido() {
+    const nome = document.querySelector('.name')
+    nome.innerHTML = "Matheus Kamers Andrade, vulgo Beiço"
+}
+
+function esconderApelido() {
+    const nome = document.querySelector('.name')
+    nome.innerHTML = "Matheus Kamers Andrade"
+}
+
